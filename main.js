90ef537939eabc2631d8c37994b97ecf693f74dc
Veerwhitehat <92624033+Veerwhitehat@users.noreply.github.com>
2022-05-04 17:22:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -34,3 +34,49 @@ var utter=new SpeechSynthesisUtterance(speak_data1+speak_data2);
 synth.speak(utter);
 
 }
+function check(){
+  img= document.getElementById("captured_img");
+  classifier.classify(img,gotResult);
+}
+function gotResult(error,results) {
+ 
+  if (error) {
+
+    console.error(error)
+    
+  } else {
+    
+    console.log(results);
+  document.getElementById("result_emotion_name1").innerHTML=results[0].label;
+  document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+
+  prediction1= results[0].label;
+  prediction2= results[1].label;
+
+  speak()
+
+  if (results[0].label=="Cheese") {
+    document.getElementById("update_emoji1").innerHTML="✌";
+  }
+  if (results[0].label=="Hello") {
+    document.getElementById("update_emoji1").innerHTML="✋";
+  }
+  if (results[0].label=="Great") {
+    document.getElementById("update_emoji1").innerHTML="✊";
+  }
+
+
+
+  if (results[1].label=="Cheese") {
+    document.getElementById("update_emoji2").innerHTML="✌";
+  }
+  if (results[1].label=="Hello") {
+    document.getElementById("update_emoji2").innerHTML="✋";
+  }
+  if (results[1].label=="Great") {
+    document.getElementById("update_emoji2").innerHTML="✊";
+  }
+  }
+  
+  
+}
